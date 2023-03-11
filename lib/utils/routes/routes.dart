7087d9ee3c106ba98413e7653ebc2afdd685eb91
NafeesTariq <codingzone43@gmail.com>
2023-03-11 22:37:11 +0500

    Doneeeee

--- lib/utils/routes/routes.dart
+++ lib/utils/routes/routes.dart
@@ -1,28 +1,11 @@
 import 'package:flutter/material.dart';
 import 'package:mvvm_project/utils/routes/rotes_named.dart';
-import 'package:mvvm_project/views/Screens/Splash.dart';
-import 'package:mvvm_project/views/Screans/BuyPointScrean.dart';
 
-import '../../views/ExtraScreens/HomeScreen_view.dart';
-import '../../views/ExtraScreens/Signup_view.dart';
-import '../../views/ExtraScreens/login_view.dart';
+import 'package:mvvm_project/views/Screans/BuyPointScrean.dart';
 
 class Routes {
   static Route<dynamic> generateRoute(RouteSettings settings) {
     switch (settings.name) {
-      case RoutesName.splash:
-        return MaterialPageRoute(
-            builder: (BuildContext context) => const Splash());
-      case RoutesName.login:
-        return MaterialPageRoute(
-            builder: (BuildContext context) => const LoginScreen());
-
-      case RoutesName.home:
-        return MaterialPageRoute(
-            builder: (BuildContext context) => const HpomeScreen());
-      case RoutesName.Signup:
-        return MaterialPageRoute(
-            builder: (BuildContext context) => const SignupView());
       case RoutesName.BuyPoints:
         return MaterialPageRoute(
             builder: (BuildContext context) => const BuyPointsScrean());
